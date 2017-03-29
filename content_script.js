526dafd08a5b31c67cf6a8f1e07ac2f5474faa06
JonLuca DeCaro <jdecaro@usc.edu>
2017-03-28 18:18:42 -0700

    WebReg Works

--- content_script.js
+++ content_script.js
@@ -20,9 +20,26 @@ $(document).ready(function() {
 });
 
 var url_template = "http://www.ratemyprofessors.com/ShowRatings.jsp?tid=";
+var empty_span = '<span class=\"instr_alt1 empty_rating col-xs-12 col-sm-12 col-md-1 col-lg-1\"><span class=\"hidden-lg hidden-md visible-xs-* visible-sm-* table-headers-xsmall\">Prof. Rating: </span></span>';
 
 
 function parseWebReg(professor_ratings) {
+
+    //Sets CSS of page to display everything correctly
+    $(".rm_alt1").css({
+        "width": "4%"
+    });
+    $(".btnAddToMyCourseBin_alt1").css({
+        "width": "8%"
+    });
+
+    $(".rm_alt0").css({
+        "width": "4%"
+    });
+    $(".btnAddToMyCourseBin_alt0").css({
+        "width": "8%"
+    });
+
 
     var total_spots = 0;
     var available_spots = 0;
@@ -40,23 +57,34 @@ function parseWebReg(professor_ratings) {
 
         hidden_total_spots = 0;
         hidden_available_spots = 0;
+
+        //Insert Prof Rating column
+        var header = $(this).find(".section_head_alt1");
+        var days = $(header).find(".instr_alt1, .instr_alt0")[0];
+        $(days).after("<span class=\"instr_alt1 col-md-1 col-lg-1\"><b>Prof. Rating</b></span>");
+
         //insert prof rating
         var only_lab = true;
-        var sections = $(this).find(".section_alt1, section_alt0").each(function() {
+        //Ever row
+        var sections = $(this).find(".section_alt1, .section_alt0").each(function() {
 
-
-            var registration_numbers_element = $(this).find(".regSeats_alt1");
+            var add_to_cb = $(this).find(".addtomycb");
+            if (add_to_cb.length != 0) {
+                add_to_cb = add_to_cb[0];
+                $(add_to_cb).attr('value', 'Add');
+            }
+            var registration_numbers_element = $(this).find(".regSeats_alt1, regSeats_alt0");
             var registration_numbers;
             if (registration_numbers_element.length != 0) {
-                registration_numbers = registration_numbers_element[0].textContent.replace("Registered:  ", "");
+                registration_numbers = registration_numbers_element[0].textContent.replace("Registered: ", "");
 
                 registration_numbers = registration_numbers.split("of");
                 //first number in # of #
-                var current_enrolled = parseInt(registration_numbers[0]);
-                var total_available = parseInt(registration_numbers[1]);
+                var current_enrolled = parseInt(registration_numbers[0].trim());
+                var total_available = parseInt(registration_numbers[1].trim());
 
                 //See if we want to count these elements
-                var class_type_element = $(this).find('.type_alt1');
+                var class_type_element = $(this).find('.type_alt1, .type_alt0');
                 var class_type;
                 if (class_type_element.length != 0) {
                     class_type = class_type_element[0].textContent;
@@ -71,14 +99,11 @@ function parseWebReg(professor_ratings) {
                     } else {
                         only_lab = false;
                     }
-
                 }
-
-
             }
 
             //check if instructor element exists, find it
-            var instructor_name_element = $(this).find(".instr_alt1");
+            var instructor_name_element = $(this).find(".instr_alt1, .instr_alt0");
             if (instructor_name_element.length != 0) {
                 //get all professor names in a hacky way
                 var instructor_names = instructor_name_element[0].innerHTML.split("span>");
@@ -89,6 +114,11 @@ function parseWebReg(professor_ratings) {
                 for (var i = 0; i < instructor_name.length; i++) {
                     //single instructor name, comma delimeted
                     single_instructor = instructor_name[i];
+                    if (single_instructor.trim() == "" && !$(this).hasClass("blank_rating")) {
+                        $(this).addClass("blank_rating");
+                        var location_of_insert = $(this).find('.instr_alt1, .instr_alt0')[0];
+                        $(location_of_insert).after(empty_span);
+                    }
                     var actual_name = single_instructor.split(", ");
                     //generate actual name
                     actual_name = actual_name[1] + " " + actual_name[0];
@@ -98,15 +128,33 @@ function parseWebReg(professor_ratings) {
                         var url = url_template + professor_ratings[actual_name].id;
 
                         if ($(this).find('.rating').length == 0) {
-                            $(this).find('.instr_alt1').append('<a class=\"rating\" href=' + url + " target=\"_blank\"> - " + professor_ratings[actual_name].rating + '</a></td>');
+                            $(this).addClass("blank_rating");
+                            //long string but needs to be exactly formatted
+                            var location_of_insert = $(this).find('.instr_alt1')[0];
+                            $(location_of_insert).after('<span class=\"hours_alt1 text-center col-xs-12 col-sm-12 col-md-1 col-lg-1\"><span class=\"hidden-lg hidden-md visible-xs-* visible-sm-* table-headers-xsmall\">Prof. Rating: </span><a class=\"rating\" href=' + url + " target=\"_blank\">" + professor_ratings[actual_name].rating + '</a></span>');
+                            if ($(this).find(".empty_rating").length != 0) {
+                                $(this).find(".empty_rating")[0].remove();
+                            }
                         } else {
                             $(this).find('.rating').append(', <a href=' + url + ">" + professor_ratings[actual_name].rating + '</a>');
+                        }
+                    } else {
+                        if (!$(this).hasClass("blank_rating")) {
+                            $(this).addClass("blank_rating");
+                            var location_of_insert = $(this).find('.instr_alt1, .instr_alt0')[0];
+                            $(location_of_insert).after(empty_span);
                         }
                     }
                 //do nothing otherwise
                 }
+
+
             } else {
-                //jQuery way of saying continue;
+                if (!$(this).hasClass("blank_rating")) {
+                    $(this).addClass("blank_rating");
+                    var location_of_insert = $(this).find('.instr_alt1, .instr_alt0')[0];
+                    $(location_of_insert).after(empty_span);
+                } //jQuery way of saying continue;
                 return true;
             }
         });
